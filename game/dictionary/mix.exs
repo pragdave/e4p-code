d8d00233298a857b1469ad06f828730416739229
pragdave <dave@pragdave.me>
2017-05-16 10:49:53 -0500

    initial import of "our first application" dictionary

---
+++ game/dictionary/mix.exs
@@ -0,0 +1,22 @@
+defmodule Dictionary.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app:     :dictionary,
+      version: "0.1.0",
+      elixir:  ">= 1.4.0",
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    []
+  end
+end
