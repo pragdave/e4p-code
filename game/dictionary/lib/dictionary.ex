520452ed36428688a6dd0db78639745637479d65
pragdave <dave@pragdave.me>
2017-06-01 12:19:02 -0500

    refacto dictionary

--- game/dictionary/lib/dictionary.ex
+++ game/dictionary/lib/dictionary.ex
@@ -1,14 +1,8 @@
 defmodule Dictionary do
 
-  def random_word() do
-    word_list()
-    |> Enum.random()
-  end
-  
-  def word_list do
-    "../assets/words.txt"
-    |> Path.expand(__DIR__)
-    |> File.read!()
-    |> String.split(~r/\n/)
-  end
+  alias Dictionary.WordList
+
+  defdelegate start(), to: WordList, as: :word_list
+  defdelegate random_word(word_list), to: WordList
+
 end
