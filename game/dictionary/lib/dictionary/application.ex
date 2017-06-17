1e0e3f030bd1048b88e224733b70d1fe9e5d0079
pragdave <dave@pragdave.me>
2017-06-17 14:23:32 -0500

    Supervise dictionary

--- game/dictionary/lib/dictionary/application.ex
+++ game/dictionary/lib/dictionary/application.ex
@@ -1,9 +1,20 @@
 defmodule Dictionary.Application do
-
+  
   use Application
-
-  def start_link(_type, _args) do
-    Dictionary.WordList.start_link()
+  
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+    
+    children = [
+      worker(Dictionary.WordList, []),
+    ]
+    
+    options = [
+      name:     Dictionary.Supervisor,
+      strategy: :one_for_one,
+    ]
+    
+    Supervisor.start_link(children, options)
   end
 
 end
