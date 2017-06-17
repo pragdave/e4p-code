3a1d8085cf7770f45d6c7b00c2a87a7fa3691c80
pragdave <dave@pragdave.me>
2017-06-17 15:03:53 -0500

    typo

--- game/dictionary/lib/dictionary/word_list.ex
+++ game/dictionary/lib/dictionary/word_list.ex
@@ -3,7 +3,7 @@ defmodule Dictionary.WordList do
   @me __MODULE__
   
   def start_link() do
-    Agent.start_link(&word_list/0, name: @md)
+    Agent.start_link(&word_list/0, name: @me)
   end
   
   def random_word() do
