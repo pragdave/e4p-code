ee136a2051f1b4906dac0bdadcd9d0503c48222f
pragdave <dave@pragdave.me>
2017-06-17 12:19:34 -0500

    agentify dictionary

--- game/dictionary/lib/dictionary/word_list.ex
+++ game/dictionary/lib/dictionary/word_list.ex
@@ -1,8 +1,11 @@
 defmodule Dictionary.WordList do
 
-  def random_word(word_list) do
-    word_list
-    |> Enum.random()
+  def start_link() do
+    Agent.start_link(&word_list/0)
+  end
+  
+  def random_word(agent) do
+    Agent.get(agent, &Enum.random/1)
   end
   
   def word_list do
